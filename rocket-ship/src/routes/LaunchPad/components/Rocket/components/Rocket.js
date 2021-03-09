4a66e3a915c6733c4257cdb2c799bd426428702a
Maria <maria.mihaila.17@gmail.com>
2021-03-09 14:36:57 +0200

    Replaced function with class component that extends PureComponent

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,10 +1,21 @@
 import React, { useState, Component, PureComponent } from 'react';
 import RocketCore from './RocketCore';
 
-export function FunctionalRocket() {
-  const [initialLaunchTime] = useState(Date.now());
 
-  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+export class FunctionalRocket extends PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      initialLaunchTime: Date.now()
+    }
+  }
+
+  render() {
+    const { initialLaunchTime } = this.state;
+
+    return <RocketCore initialLaunchTime={this.initialLaunchTime} />
+  }
 }
 
 
