f0f57081cc3325d7ea3959710bb824cab0cd1b2d
Maria <maria.mihaila.17@gmail.com>
2021-03-09 15:18:44 +0200

    Import ThemeMode and add component to the App.js file

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
+import ThemeMode from './DarkMode';
 
 function App() {
   return (
     <div className="app">
       <div className="level">
         <div>
+
           <h1 className="title">Dark Mode Challenge</h1>
         </div>
-
+          <ThemeMode/>
         {/* --The button that should toggle dark mode-- */}
         <button className="app__dark-mode-btn icon level-right">
           <FontAwesomeIcon icon={faMoon} />
